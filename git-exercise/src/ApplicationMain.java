323838c27bc887f0fd7951a0f3e42e00ace40355
Stephanie Hernandez <68871971+St3phy831@users.noreply.github.com>
2022-09-15 10:54:19 -0700

    Added a title to output

--- git-exercise/src/ApplicationMain.java
+++ git-exercise/src/ApplicationMain.java
@@ -12,7 +12,9 @@ public class ApplicationMain {
 				"Milk"
 		};
 		
-		for (String item: shopList) {
+		System.out.println("My Shopping List");
+             	System.out.println();
+             	for (String item: shopList) {
 			System.out.println(item);
 		}	
        }
